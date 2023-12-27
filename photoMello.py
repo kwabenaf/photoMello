bb5eaf4ce6584aa99f37dab3fd4844dc0c8d0fec
kwabs <fordjour.28@gmail.com>
2023-12-27 22:05:04 +0000

    improved import. now supports video and relevate jpg files. quality of life fixes too

--- photoMello.py
+++ photoMello.py
@@ -140,42 +140,92 @@ class PhotoMello:
             def copy_file(source_path, destination_path):
                 shutil.copy(source_path, destination_path)
 
-            # Function to import photos
-            def import_photos_internal():
+        # Function to import media files (DNG, JPG, and AVI)
+        def import_media_internal():
+            # Get a set of files in all additional folders
+            additional_files = set()
+            for folder in self.additional_folders.get('laptop' if on_laptop() else 'pc', []):
+                if os.path.exists(folder):
+                    for root, dirs, files in os.walk(folder):
+                        additional_files.update(files)
 
-                # Get a set of files in all additional folders
-                additional_files = set()
-                for folder in self.additional_folders.get('laptop' if on_laptop() else 'pc', []):
-                    if os.path.exists(folder):
-                        for root, dirs, files in os.walk(folder):
-                            additional_files.update(files)
+            # Get a set of files in the destination folder
+            destination_files = set(os.listdir(destination_folder))
 
-                # Get a set of files in the destination folder
-                destination_files = set(os.listdir(destination_folder))
+            # Track the count of copied files for each type
+            dng_count = 0
+            jpg_count = 0
+            video_count = 0
 
-                # Iterate through files in the source folder and its subdirectories
-                for root, dirs, files in os.walk(source_folder):
-                    for filename in files:
-                        if filename.lower().endswith('.dng') and not self.cancel_operation:
-                            source_path = os.path.join(root, filename)
+            # Function to check and copy file if not duplicate
+            def check_and_copy_file(destination_path, filename, destination_files, additional_files, source_path):
+                nonlocal dng_count, jpg_count, video_count
+                if filename in destination_files or filename in additional_files:
+                    print(f"Skipping duplicate file: {filename}")
+                else:
+                    # Copy the file to the destination folder
+                    copy_file(source_path, destination_path)
+                    print(f"File copied: {filename}")
+
+                    # Increment the count based on the file type
+                    if filename.lower().endswith('.dng'):
+                        dng_count += 1
+                    elif filename.lower().endswith('.jpg'):
+                        jpg_count += 1
+                    elif filename.lower().endswith('.avi'):
+                        # Check if a video file with the same name already exists in the destination folder
+                        if filename not in destination_files:
+                            video_count += 1
+                            print(f"Video file copied: {filename}")
+
+            # Iterate through files in the source folder and its subdirectories
+            for root, dirs, files in os.walk(source_folder):
+                for filename in files:
+                    if not self.cancel_operation:
+                        source_path = os.path.join(root, filename)
+
+                        # Check if the file is a .DNG, .JPG, or .AVI file
+                        if filename.lower().endswith('.dng'):
                             destination_path = os.path.join(destination_folder, filename)
+                            check_and_copy_file(destination_path, filename, destination_files, additional_files, source_path)
+                        elif filename.lower().endswith('.jpg'):
+                            dng_filename = filename[:-4] + '.dng'
+                            dng_path = os.path.join(root, dng_filename)
+                            if not os.path.exists(dng_path):
+                                destination_path = os.path.join(destination_folder, filename)
+                                check_and_copy_file(destination_path, filename, destination_files, additional_files, source_path)
+                                print(f"JPG file copied: {filename}")
+                        elif filename.lower().endswith('.avi'):
+                            video_destination_folder = os.path.join(destination_folder, 'vid')
+                            os.makedirs(video_destination_folder, exist_ok=True)
+                            destination_path = os.path.join(video_destination_folder, filename)
+                            check_and_copy_file(destination_path, filename, destination_files, additional_files, source_path)
+                            print(f"Video file copied: {filename}")
 
-                            # Check if the file already exists in the other folders
-                            if filename in destination_files or filename in additional_files:
-                                print(f"Skipping duplicate file: {filename}")
-                            else:
-                                # Copy the .DNG file to the destination folder
-                                copy_file(source_path, destination_path)
-                                print(f"File copied: {filename}")
+            if not self.cancel_operation:
+                # Generate the final message based on the counts
+                message = f"Operation complete."
+                if dng_count > 0:
+                    message += f" {dng_count} .DNG file(s) copied."
+                if jpg_count > 0:
+                    message += f" {jpg_count} .JPG file(s) copied."
+                if video_count > 0:
+                    message += f" {video_count} video file(s) copied."
 
-                if not self.cancel_operation:
-                    tk.messagebox.showinfo("Info", f"Operation complete. {len(os.listdir(destination_folder))} .DNG files copied to the destination folder.")
+                # Display the message
+                tk.messagebox.showinfo("Info", message)
+            else:
+                # Display a message if no files were copied
+                tk.messagebox.showinfo("Info", "Operation canceled. No new files copied.")
 
-            # Listen for the "ESC" key press to cancel the operation
-            keyboard.on_press_key('esc', check_for_cancel)
 
-            # Import photos from the source folder to the destination folder
-            import_photos_internal()
+        # Listen for the "ESC" key press to cancel the operation
+        keyboard.on_press_key('esc', check_for_cancel)
+
+        # Import media files from the source folder to the destination folder
+        import_media_internal()
+
+
 
 
 # ORGANISE PHOTOS
