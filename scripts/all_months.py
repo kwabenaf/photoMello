884162a63dbeb56bcf49a8f51d50e29fae961bc3
kwabs <fordjour.28@gmail.com>
2023-12-27 22:27:32 +0000

    includes jpg files now

--- scripts/all_months.py
+++ scripts/all_months.py
@@ -2,24 +2,35 @@ import os
 import shutil
 from datetime import datetime
 import exifread
+from tkinter import filedialog
+import socket
+
+def on_laptop():
+    hostname = socket.gethostname()
+    return 'laptop' in hostname.lower()
+
+def get_folders():
+    return (
+        r"G:\Other computers\My computer\ricoh\all"
+        if on_laptop()
+        else r"D:\ricoh\all"
+    )
 
 def get_original_date(file_path):
     with open(file_path, 'rb') as file:
         tags = exifread.process_file(file, details=False)
         date_str = tags.get('EXIF DateTimeOriginal')
-        if date_str:
-            return datetime.strptime(str(date_str), '%Y:%m:%d %H:%M:%S')
-        return None
+        return datetime.strptime(str(date_str), '%Y:%m:%d %H:%M:%S') if date_str else None
 
-def organize_dng_files(src_folder, dest_folder):
+def organise_image_files(src_folder, dest_folder):
     files_moved_count = 0
     moved_files = set()
 
     for root, dirs, files in os.walk(src_folder):
         for file in files:
-            if file.lower().endswith('.dng'):
+            if file.lower().endswith('.dng') or file.lower().endswith('.jpg'):
                 file_path = os.path.join(root, file)
-                if file_path not in moved_files:  # Check if the file has already been moved
+                if file_path not in moved_files:
                     original_date = get_original_date(file_path)
                     if original_date:
                         dest_path = os.path.join(dest_folder, str(original_date.year), f"{original_date.month:02d}")
@@ -32,9 +43,22 @@ def organize_dng_files(src_folder, dest_folder):
     return files_moved_count
 
 if __name__ == "__main__":
-    src_folder = r'D:\ricoh\all'
-    dest_folder = r'D:\ricoh\all'
+    
+    # Get source folder based on the hostname
+    src_folder = get_folders()
 
-    print("Organizing DNG files...")
-    total_files_moved = organize_dng_files(src_folder, dest_folder)
-    print(f"Organizing complete. {total_files_moved} files moved.")
+    # Ask the user to select the destination folder
+    dest_folder = filedialog.askdirectory(title="Select Destination Folder")
+
+
+    if src_folder:
+        # Combine the source folder with the additional folders
+        all_folders = (src_folder) + get_folders()
+
+        # Organise images in all selected folders
+        for folder in all_folders:
+            print(f"Organising DNG and JPG files in {folder}...")
+            total_files_moved = organise_image_files(folder, dest_folder)
+            print(f"Organising complete. {total_files_moved} files moved.")
+    else:
+        print("Source folder not selected.")
